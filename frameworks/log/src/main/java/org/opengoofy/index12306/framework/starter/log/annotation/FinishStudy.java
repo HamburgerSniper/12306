46feb0af34c851c2fc57f816caef605a921a07f1
liuxuanchen.66 <liuxuanchen.66@bytedance.com>
2023-12-13 16:22:43 +0800

    doc: add @FinishStudy enum

---
+++ frameworks/log/src/main/java/org/opengoofy/index12306/framework/starter/log/annotation/FinishStudy.java
@@ -0,0 +1,15 @@
+package org.opengoofy.index12306.framework.starter.log.annotation;
+
+
+import org.opengoofy.index12306.framework.starter.log.constants.FinishStudyEnum;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FinishStudy {
+    FinishStudyEnum status();
+}
---
+++ frameworks/log/src/main/java/org/opengoofy/index12306/framework/starter/log/constants/FinishStudyEnum.java
@@ -0,0 +1,7 @@
+package org.opengoofy.index12306.framework.starter.log.constants;
+
+public enum FinishStudyEnum {
+    TRUE,
+
+    FALSE;
+}
